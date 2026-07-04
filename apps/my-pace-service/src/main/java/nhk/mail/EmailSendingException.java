d6e5df5181d511226ae88ce43926eded2c7d48dc
Hoang Khang <nguyenhoangkhq7@gmail.com>
2026-07-04 09:12:29 +0700

    chang sender mail and fix cors

---
+++ apps/my-pace-service/src/main/java/nhk/mail/EmailSendingException.java
@@ -0,0 +1,11 @@
+package nhk.mail;
+
+public class EmailSendingException extends RuntimeException {
+    public EmailSendingException(String message) {
+        super(message);
+    }
+
+    public EmailSendingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
