393e3f51954fc8348f20aa2817fed13f9ae7c9a3
Hoang Khang <nguyenhoangkhq7@gmail.com>
2026-07-10 15:37:40 +0700

    fix check trivy

--- apps/my-pace-service/src/main/java/nhk/goal/GoalMapper.java
+++ apps/my-pace-service/src/main/java/nhk/goal/GoalMapper.java
@@ -4,7 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface GoalMapper {
     GoalDto toDto(Goal goal);
     Goal toEntity(GoalCreateRequest request);
--- apps/my-pace-service/src/main/java/nhk/task/TaskMapper.java
+++ apps/my-pace-service/src/main/java/nhk/task/TaskMapper.java
@@ -4,7 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", uses = {nhk.category.CategoryMapper.class}, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring", uses = {nhk.category.CategoryMapper.class}, unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TaskMapper {
     TaskDto toDto(Task task);
     Task toEntity(TaskCreateRequest request);
