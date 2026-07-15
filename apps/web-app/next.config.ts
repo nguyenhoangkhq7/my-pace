779a186072b2651e3fc22ea52f48f0f23b7fc433
Hoang Khang <nguyenhoangkhq7@gmail.com>
2026-07-15 19:43:57 +0700

    fix config vercel deploy

--- apps/web-app/next.config.ts
+++ apps/web-app/next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/:path*`, // Proxy to Backend
+        destination: `${process.env.API_URL || 'http://localhost:8080/api'}/:path*`, // Proxy to Backend
       },
     ];
   },
